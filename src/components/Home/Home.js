2f57e80469480bf35ba14bff43c515420d538f24
mik5owski <piatkowskimikolaj@gmail.com>
2019-10-16 18:18:44 +0200

    added some styling to home component and moved styling variables to a separate file

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react"
+import photo from './cat.jpg'
+import './Home.css'
 
 class Home extends Component {
 
     render() {
         return (
-            <div>Home</div>
+            <div className="home-wrapper">
+                <div className="image-wrapper">
+                    <img className="photo" src={photo} alt="Mikołaj"></img>
+                </div>
+                <div className="text-wrapper">
+                    <h2 className="greeting">Hello there!</h2>
+                    <h2 className="introduction">I'm Nicolas and I'm more than happy to welcome you to my portfolio.</h2>
+                </div>
+            </div>
         )
     }
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -9,13 +9,13 @@ class Nav extends Component {
             <div className="nav-wrapper">
                 <nav className="navbar">
                     <span className="title">
-                        <NavLink className="name" exact to="/">Mikołaj Piątkowski</NavLink>
+                        <NavLink className="name" activeClassName="active" exact to="/">Mikołaj Piątkowski</NavLink>
                         <p className="subtitle">/Front-End Developer</p>
                     </span>
                     <ul className="navbar-list">
-                        <li activeClassName="active" className="navbar-list-item"><NavLink className="home" exact to="/">About</NavLink></li>
+                        <li className="navbar-list-item"><NavLink className="home" activeClassName="active" exact to="/">About</NavLink></li>
                         <li className="navbar-list-item">
-                            <NavLink activeClassName="active" className="projects" exact to="/Projects">Projects</NavLink>
+                            <NavLink className="projects" activeClassName="active" exact to="/Projects">Projects</NavLink>
                         </li>
                         <li className="navbar-list-item">
                             <a className="resume">Résumé</a>
