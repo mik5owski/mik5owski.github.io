af5162dd5d3d4966e964f1405d2ce7cd930448e3
mik5owski <piatkowskimikolaj@gmail.com>
2019-11-26 13:13:39 +0100

    worked on Home component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react"
-
+import './Contact.css'
 class Contact extends Component {
 
     render() {
         return (
-            <div>Contact</div>
+            <div className="contact-wrapper">
+                <div className="contact-content">
+                    <ul className="contact-list">
+                        <li className="contact-list-item">Email</li>
+                        <li className="contact-list-item">Facebook</li>
+                        <li className="contact-list-item">Linkedin</li>
+                        <li className="contact-list-item">Resume</li>
+                    </ul>    
+                </div>
+            </div>
         )
     }
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
-import photo from './cat.jpg'
+import photo from './computer.jpeg'
+import ballImage from './ball.jpeg'
 import './Home.css'
 
 class Home extends Component {
@@ -11,8 +12,52 @@ class Home extends Component {
                     <img className="photo" src={photo} alt="Mikołaj"></img>
                 </div>
                 <div className="text-wrapper">
-                    <h2 className="greeting">Hello there!</h2>
-                    <h2 className="introduction">I'm Nicolas and I'm more than happy to welcome you to my portfolio.</h2>
+                    <h3 className="greeting">Hello there! I'm Nicolas, a <strong className="important">Front-End dev</strong> with educational background in mechatronics.</h3>
+                    <h4 className="about">I turned to programming last year and decided to focus on it more this year. I was always on and off with programming thanks to my dad being a programmer and me using my own programs during my studies. But only last year when he gave me a book on JavaScript and I created my first little game, I knew this is it. I enrolled in infoShare Academy where they taught me a lot about building web pages, apps and games, using React, incorporating REST, AJAX, Redux, TypeScript, working with GIT and all that good jazz. Now that I'm finished with studying all of it, I think it's time to put that knowledge to use, so if you are intrested in working with me or me working for you, please be my guest and reach out. I'm looking forward to hearing from you.</h4>
+                </div>
+                <div className="hobbies-wrapper">
+                    <div className="hobby">
+                        <div className="hobby-description">
+                            <div className="hobby-description-text">
+                                Lorem ipsum
+                            </div>
+                            <div className="hobby-description-button">
+                                <button>GO TO SiTE</button>
+                            </div>
+                        </div>
+                        <div className="hobby-image-wrapper">
+                            <img className="hobby-image" src={ballImage}>
+                            </img>
+                        </div>
+                    </div>
+                    <div className="hobby">
+                        <div className="hobby-image-wrapper">
+                            <img className="hobby-image" src={ballImage}>
+                            </img>
+                        </div>
+                        <div className="hobby-description">
+                            <div className="hobby-description-text">
+                                Lorem ipsum
+                            </div>
+                            <div className="hobby-description-button">
+                                <button>GO TO SiTE</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="hobby">
+                        <div className="hobby-description">
+                            <div className="hobby-description-text">
+                                Lorem ipsum
+                            </div>
+                            <div className="hobby-description-button">
+                                <button>GO TO SiTE</button>
+                            </div>
+                        </div>
+                        <div className="hobby-image-wrapper">
+                            <img className="hobby-image" src={ballImage}>
+                            </img>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
